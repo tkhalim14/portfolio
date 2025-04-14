5c3fdce28471e3368c7ad12b0f89912091855f3e
Tabish Khalid Halim <tkhalim14@gmail.com>
2025-04-14 17:15:04 +0530

    Updated Resume Link

--- src/app/contactMe.js
+++ src/app/contactMe.js
@@ -2,7 +2,7 @@ const socialMediaLinks = {
     'Github':'https://github.com/tkhalim14',
     'Gmail':'mailto:tkhalim14@gmail.com',
     'Instagram':'https://www.instagram.com/thesnagtalker007/',
-    'Resume':'https://drive.google.com/file/d/1HIv8yDgDJ7vnKK28P0PQYBNSH1dqqSZb/view?usp=sharing',
+    'Resume':'https://drive.google.com/file/d/1yyR_aFOmmxuSQa3mrZzdupTi3-wGQQ8Y/view?usp=sharing',
     'Linkedin': 'https://www.linkedin.com/in/tabish-khalid-halim-27209b201/',
     'Facebook': 'https://www.facebook.com/tabish.khalidhalim/'
 };
